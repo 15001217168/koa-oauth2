7fd9a467a55434440dc33a094f6b61eb3e7b8ef0
刘雪松 <1076239475@qq.com>
2018-03-07 14:20:57 +0800

    添加redis

---
+++ conf/conf.js
@@ -0,0 +1,16 @@
+module.exports = {
+    redis: {
+        dev: {
+            host: '127.0.0.1',
+            port: '12345',
+            username: 'dev',
+            pwd: 'dev'
+        },
+        product: {
+            host: '127.0.0.1',
+            port: '12345',
+            username: 'product',
+            pwd: 'product'
+        }
+    }
+};
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const OAuth = require('./lib/oauth');
 
+global.redis_env = process.env.node_env === 'development' ? 'dev' : 'product';
+
 module.exports = function(options, app) {
     let oauth = new OAuth();
     app.use(oauth.auth);
